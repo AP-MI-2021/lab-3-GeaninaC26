6acb9768e97723df7d87f41fe827718839529159
Geanina <gianinaclipa@hmail.com>
2021-10-13 11:36:13 +0300

    Completare lab 3

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def show_menu() :
     print('Alege optiunea: ')
     print('1.Cea mai lunga secventa de numere prime.')
     print('2.Cea mai lunga secventa de numere formate din numere prime ')
+    print('3.Cea mai lunga secventa de numere pare.')
+    print('4.Cea mai lunca secventa de numere palindrom.')
 
 def read_list() -> List[int] :
     '''
@@ -17,6 +19,25 @@ def read_list() -> List[int] :
         lst.append(int(num_str))
     return lst
 
+def is_palindrome(n):
+    '''
+    Verificam daca numarul dat este palindrom
+    Input:
+    -x, numar natural, dat
+    Output:
+    -False, daca numarul nu este palindrom
+    -True, daca numarul este palindrom
+    '''
+    temp = n
+    reverse = 0
+    while n > 0:
+        cifra = n % 10
+        reverse = reverse * 10 + cifra
+        n = n // 10
+    if temp == reverse :
+        return True
+    else:
+        return False
 
 def is_prime(nr) ->int :
     """
@@ -91,6 +112,49 @@ def get_longest_prime_digits(lst: list[int]) -> list[int] :
                     secventa_maxima = lst[st: dr + 1]
     return secventa_maxima
 
+def get_longest_all_even(lst: list[int]) -> list[int] :
+    """
+    Gaseste si returneaza cea mai lunga secventa de numere pare din lista data.
+    :param lst: lista in care se cauta secventa cu proprietatea ceruta
+    :return: secventa maxima care indeplineste proprietatea ceruta
+    """
+    secventa_maxima = []
+    n = len(lst)
+    all_even = True
+    for st in range(n):
+        for dr in range(st, n):
+            for num in range(st, dr+1):
+                all_even = True
+                if lst[num] % 2 == 1:
+                    all_even = False
+                    break
+            if all_even:
+                if dr - st + 1 > len(secventa_maxima):
+                    secventa_maxima = lst[st: dr + 1]
+    return secventa_maxima
+
+
+def get_longest_all_palindromes(lst: list[int]) -> list[int] :
+    """
+    Gaseste si returneaza cea mai lunga secventa de numere formate din numere palindrom din lista data.
+    :param lst: lista in care se cauta secventa cu proprietatea ceruta
+    :return: secventa maxima care indeplineste proprietatea ceruta
+    """
+    secventa_maxima = []
+    n = len(lst)
+    all_palindromes = True
+    for st in range(n):
+        for dr in range(st, n):
+            for num in range(st, dr+1):
+                all_palindromes = True
+                if not is_palindrome(lst[int(num)]):
+                    all_palindromes = False
+                    break
+            if all_palindromes:
+                if dr - st + 1 > len(secventa_maxima):
+                    secventa_maxima = lst[st: dr + 1]
+    return secventa_maxima
+
 def test_get_longest_all_primes() :
     assert get_longest_all_primes([2, 3, 5, 10, 13, 11]) == [2, 3, 5]
     assert get_longest_all_primes([2, 3, 5, 10, 13, 11, 13 ,41, 29, 100]) == [13, 11, 13, 41, 29]
@@ -103,6 +167,15 @@ def test_get_longest_prime_digits():
     assert get_longest_prime_digits([2, 3, 5, 7, 23 ]) == [2, 3, 5, 7, 23]
     assert get_longest_prime_digits([0, 0, 1, 1, 0, 1]) == []
 
+def test_get_longest_all_even() :
+    assert get_longest_all_even([12, 13, 10, 2, 8, 14]) == [10, 2, 8, 14]
+    assert get_longest_all_even([13, 1, 15, 17, 29]) == []
+    assert get_longest_all_even([2, 4, 6, 8]) == [2, 4, 6, 8]
+
+def test_get_longest_all_palindromes() :
+    assert get_longest_all_palindromes([12, 22, 242, 99, 53]) == [22, 242, 99]
+    assert get_longest_all_palindromes([12, 13, 78, 45]) == []
+    assert get_longest_all_palindromes([22, 33, 44, 565 ]) == [22, 33, 44, 565]
 
 def main() :
     lst=[]
@@ -115,6 +188,12 @@ def main() :
         elif optiune == '2' :
             lst = read_list()
             print('Cea mai lunga secventa de numere formate doar din cifre prime este :', get_longest_prime_digits(lst))
+        elif optiune == '3' :
+            lst=read_list()
+            print('Cea mai lunga secventa de numere pare este: ', get_longest_all_even(lst))
+        elif optiune == '4' :
+            lst = read_list()
+            print('Cea mai lunga secventa de numere palindrom este: ', get_longest_all_palindromes(lst))
         elif optiune == 'x' :
             break
         else :
@@ -123,4 +202,6 @@ def main() :
 if __name__ == '__main__' :
     test_get_longest_all_primes()
     test_get_longest_prime_digits()
+    test_get_longest_all_even()
+    test_get_longest_all_palindromes()
     main()
